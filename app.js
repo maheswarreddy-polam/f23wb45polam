7f4e6ead1a1adf58941db49fd010c6132a8467d8
Polam <S562045@nwmissouri.edu>
2023-10-31 13:48:10 -0500

    class photos

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eagleRouter = require('./routes/eagle');
 var boardRouter = require('./routes/board');
-
+var chooseRouter = require('./routes/choose');
 var app = express();
 
 // view engine setup
@@ -25,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/eagle', eagleRouter);
 app.use('/board', boardRouter);
-
+app.use('/choose', chooseRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
